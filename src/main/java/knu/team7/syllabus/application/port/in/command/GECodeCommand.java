04e7cdbe35ea107504f87012e0a82dc958cbaaa5
hjinshin <gudwls818@gmail.com>
2024-05-24 00:34:18 +0900

    clear

--- src/main/java/knu/team7/syllabus/application/port/in/command/GECodeCommand.java
+++
@@ -1,7 +0,0 @@
-package knu.team7.syllabus.application.port.in.command;
-
-import lombok.Builder;
-
-@Builder
-public record GECodeCommand(String codeId, String codeNm, String upperCodeId, String upperCodeNm) {
-}
