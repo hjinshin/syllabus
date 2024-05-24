51b27d5b5ab7c5c34c3800513163e521167de789
Phonedolly <Phonedolly@users.noreply.github.com>
2024-05-24 19:25:39 +0900

    construct ISubject structure (1)

--- types/ISubject.ts
+++ types/ISubject.ts
@@ -8,6 +8,50 @@ export interface ISubject {
   building: IGeoData;
 }
 
+// 교양, 군사학, 교직, 연구산학처, 대학, 대학원, 전문대학원, 원격수업, 원격강의, 플립드러닝 강좌, 거점국립대 원격강좌, 대구경북권원격강좌, SU평가강좌, 실험실습실기강좌, KNU미래설계, 인문교양, SDG교양
+// represents in english
+export enum ESubjectType {
+  General = "General",
+  Military = "Military",
+  Teaching = "Teaching",
+  Research = "Research",
+  University = "University",
+  GraduateSchool = "GraduateSchool",
+  ProfessionalGraduateSchool = "ProfessionalGraduateSchool",
+  RemoteClass = "RemoteClass",
+  RemoteLecture = "RemoteLecture",
+  FlippedLearning = "FlippedLearning",
+  NationalUniversityRemoteLecture = "NationalUniversityRemoteLecture",
+  DaeguGyeongbukRegionRemoteLecture = "DaeguGyeongbukRegionRemoteLecture",
+  SUAssessmentCourse = "SUAssessmentCourse",
+  ExperimentalPracticeCourse = "ExperimentalPracticeCourse",
+  KNUFutureDesign = "KNUFutureDesign",
+  HumanitiesGeneral = "HumanitiesGeneral",
+  SDGGeneral = "SDGGeneral",
+}
+
+// 첨성인기초, 첨성인핵심, 첨성인일반, 첨성인소양
+export enum GeneralSubjectType {
+  Basic = "Basic",
+  Core = "Core",
+  General = "General",
+  Elective = "Elective",
+}
+
+// 독서와토론, 사고교육, 글쓰기, 외국어, 수리, 기초과학, 실용영어, 소프트웨어
+export enum HumanitiesGeneralSubjectType {
+  ReadingAndDiscussion = "ReadingAndDiscussion",
+  ThinkingEducation = "ThinkingEducation",
+  Writing = "Writing",
+  ForeignLanguage = "ForeignLanguage",
+  Mathematics = "Mathematics",
+  BasicScience = "BasicScience",
+  PracticalEnglish = "PracticalEnglish",
+  Software = "Software",
+}
+
+//
+
 export interface AssignedTime {
   day: Day[];
   times: ITime[];
